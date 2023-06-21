339259cd7de7386eaf60b86e374e4de9b3ddefee
adviti <adviti@umich.edu>
2023-06-21 09:02:37 -0400

    Comments on main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:practice_app/screens/home_page/notifications/notification_info.dart';
 import 'package:practice_app/user_state.dart';
-import 'package:color_blindness/color_blindness.dart';
-import 'package:color_blindness/color_blindness_color_scheme.dart';
+// import 'package:color_blindness/color_blindness.dart';
+// import 'package:color_blindness/color_blindness_color_scheme.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -21,13 +20,16 @@ class MyApp extends StatelessWidget {
     return FutureBuilder(
         future: _initialization,
         builder: (context, snapshot) {
+          // showing a loading screen while Firebase is initializing
           if (snapshot.connectionState == ConnectionState.waiting) {
             return const MaterialApp(
                 debugShowCheckedModeBanner: false,
                 home: Scaffold(
                     body: Center(
                         child: Center(child: CircularProgressIndicator()))));
-          } else {
+          }
+          // Firebase is initialized. Continue with app
+          else {
             return MaterialApp(
                 debugShowCheckedModeBanner: false,
                 title: 'Flutter Practice app',
@@ -66,8 +68,8 @@ class MyApp extends StatelessWidget {
                     brightness: Brightness.light,
                   ),
                 ),
-                home: const UserState() // const NotificationInfo(), //
-                );
+                // set the home screen of the app to UserState()
+                home: const UserState());
           }
         });
   }
