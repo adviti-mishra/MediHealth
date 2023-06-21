a23bec1258daddcf9a48c60dd77bfe3021b2d6ca
adviti <adviti@umich.edu>
2023-06-21 09:15:47 -0400

    Added comments to user state

--- lib/user_state.dart
+++ lib/user_state.dart
@@ -5,26 +5,38 @@ import 'package:practice_app/screens/home_page/medicine/medicine_screen.dart';
 
 class UserState extends StatelessWidget {
   const UserState({Key? key}) : super(key: key);
-   
+
   @override
   Widget build(BuildContext context) {
     return StreamBuilder(
-        // checks whether user is signed in or not - keep listening to any event depending on this
+        // Stream that checks whether user is signed in or not
+        // Listens to any changes in the authentication state
         stream: FirebaseAuth.instance.authStateChanges(),
         builder: (ctx, userSnapshot) {
+          // case 1 : user is not logged in
           if (userSnapshot.data == null) {
             print('User has not logged in yet');
+            // return Login page
             return const Login();
-          } else if (userSnapshot.hasData) {
+          }
+          // case 2 : user is already logged in
+          else if (userSnapshot.hasData) {
             print('User is already logged in in');
-            return MedicineScreen(userID: FirebaseAuth.instance.currentUser!.uid);
-          } else if (userSnapshot.hasError) {
+            // retrn Medicine page
+            return MedicineScreen(
+                userID: FirebaseAuth.instance.currentUser!.uid);
+          }
+          // case 3 : error occurs during authentication process
+          else if (userSnapshot.hasError) {
             return const Scaffold(
                 body: Center(child: Text('An error has occured')));
-          } else if (userSnapshot.connectionState == ConnectionState.waiting) {
+          }
+          // case 4 : waiting for the authentication state
+          else if (userSnapshot.connectionState == ConnectionState.waiting) {
             return const Scaffold(
                 body: Center(child: CircularProgressIndicator()));
           }
+          // case 5 : if none of the conditions are met, generic error message
           return const Scaffold(
               body: Center(child: Text('Something went wrong')));
         });
